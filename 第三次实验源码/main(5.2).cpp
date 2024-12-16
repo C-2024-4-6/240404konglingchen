efb49dd588a00bf49a9438839c2d8eb3140fc24a
KLC <991407561@qq.com>
2024-12-16 19:20:45 +0800

    Delete 第三次实验源码 directory

--- 第三次实验源码/main(5.2).cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-#include"student.h"
-int main() {
-	Student s1;
-	s1.set_value(004, "klc", 'k');
-	s1.display();
-}
--- 第三次实验源码/student(5.2).h
+++
@@ -1,9 +0,0 @@
-class Student {
-public:
-	void display();
-void set_value(int n, const char* nm, char s);
-private:
-	int num;
-	char name[20];
-	char sex;
-};
